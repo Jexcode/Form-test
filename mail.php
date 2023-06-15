fd51cdbc8d2b666b27eb9ceb61be21be125a9062
Joseph <113116981+Jexcode@users.noreply.github.com>
2023-06-16 00:41:25 +0100

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+  // Fetch form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  // Retrieve other form input fields
+
+  // Email details
+  $to = array('bialakayode@gmail.com', 'bialajosepholukayode@gmail.com', 'josephbiala@gmail.com');
+  $subject = 'Form Submission';
+  $message = "Name: $name\nEmail: $email\n"; // Include other form data in the message
+
+  // Send emails to the multiple addresses
+  foreach ($to as $recipient) {
+    mail($recipient, $subject, $message);
+  }
+
+  // Redirect the user to a thank you page
+  header('Location: thank_you.html');
+  exit();
+?>
